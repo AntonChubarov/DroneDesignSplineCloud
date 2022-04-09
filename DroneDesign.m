e8befff03ee9c2530d600b89ddb04b124b96365c
antonchubarovwd <antonc@wizardsdev.com>
2022-04-09 20:53:31 +0300

    feat: base calculation

--- DroneDesign.m
+++ DroneDesign.m
@@ -1,18 +1,17 @@
 function M0 = DroneDesign(dp)
 
-global payloadWeight specCapSlineID maxTakeoffThrotle minBatteryCapacity hoveringTime
+global payloadWeight specCapSlineID maxTakeoffThrotle minBatteryCapacity hoveringTime specCapSpl
 
 mubat=dp(1);
 muel=dp(2);
 mufr=dp(3);
-mumot=dp(4);
-muprop=dp(5);
-nCell=dp(6);
-Crate=5*dp(7);
-nprop=dp(8);
+mueng=dp(4);
+nCell=dp(5);
+Crate=5*dp(6);
+nprop=2*dp(7);
 
 
-M0=payloadWeight/(1-(mubat+muel+mufr+nprop*mumot+nprop*muprop));
+M0=payloadWeight/(1-(mubat+muel+mufr+nprop*mueng));
 if M0<=0
     M0=100*payloadWeight;
     return
@@ -21,8 +20,14 @@ end
 Mbat=mubat*M0;
 Mel=muel*M0;
 Mfr=mufr*M0;
-Mmot=mumot*M0;
-Mprop=muprop*M0;
+Mmot=mueng*M0/1.18;
+Mprop=mueng*M0*(1-1/1.18);
+
+Imotmax=3.5*sqrt(1000*Mmot);
+
+Imax=nprop*Imotmax;
+
+disp(['Max. motor current ',num2str(Imax,4),' A'])
 
 disp(['Masses: Bat. ',num2str(Mbat,2),' kg; El. ',num2str(Mel,2),' kg; Frame ',num2str(Mfr,2),...
     ' kg; Motor ',num2str(Mmot,2),' kg; Prop ',num2str(Mprop,2)])
@@ -30,7 +35,6 @@ disp(['Masses: Bat. ',num2str(Mbat,2),' kg; El. ',num2str(Mel,2),' kg; Frame ',n
 maxVoltage=nCell*3.7;
 disp(['Max. Voltage ',num2str(maxVoltage,4),' V'])
 
-specCapSpl=GetNURBSfromSC(specCapSlineID);
 specCap=getYByX(specCapSpl,Crate)*1000;
 disp(['Specific cell capacity ',num2str(specCap,4),' mAh/kg'])
 Cap=specCap*Mbat/nCell;
@@ -39,32 +43,50 @@ disp(['Capacity ',num2str(Cap,4),' mAh'])
 disp(['Charge ',num2str(Charge,4),' C'])
 
 maxCurrent=Cap*Crate/1000;
-disp(['Max. current ',num2str(maxCurrent,4),' A'])
+disp(['Max. battery current ',num2str(maxCurrent,4),' A'])
 
-tspan = [1 100];
-opts = odeset('RelTol',1e-6,'AbsTol',1e-6,'Events',@EventsFcn);
-[t,c] = ode45(@(t,c) odefun(t,c), tspan, [Charge], opts);
+if Imax > maxCurrent
+    M0=100*payloadWeight;
+    return
+end
 
-    function dcdt = odefun(t,c)
-    dcdt=-maxTakeoffThrotle*maxCurrent;
-    end
+Imotdsch=Imotmax*maxTakeoffThrotle^2.5;
 
-    function [value,isterminal,direction] = EventsFcn(t,c)
-    value=[minBatteryCapacity*c];
-    isterminal=[1];
-    direction=[-1];
-    end
+Thr=Mmot*Imotdsch^0.7;
+disp(['Motor thrust ',num2str(Thr,4),' kg'])
 
-disp(['Simulation time ', num2str(t(end),2),' s'])
-if t(end) < hoveringTime
+if 4*Thr < M0
+    M0=100*payloadWeight;
+    return
+end
+
+Idsch=4*Imotdsch;
+
+%tspan = [1 100];
+%opts = odeset('RelTol',1e-6,'AbsTol',1e-6,'Events',@EventsFcn);
+%[t,c] = ode45(@(t,c) odefun(t,c), tspan, [Charge], opts);
+
+%    function dcdt = odefun(t,c)
+%        dcdt=-maxTakeoffThrotle*Imotmax;
+%    end
+
+%    function [value,isterminal,direction] = EventsFcn(t,c)
+%        value=[minBatteryCapacity*c];
+%        isterminal=[1];
+%        direction=[-1];
+%    end
+
+simTime=(1-minBatteryCapacity)*Charge/Idsch;
+
+disp(['Simulation time ', num2str(simTime,3),' s'])
+if simTime < hoveringTime || isnan(simTime)
     M0=100*payloadWeight;
     return
 end
 
 end
 
-function nurbs = GetNURBSfromSC(curveId)
-spline = fetchCurve(curveId);
-[ctrlPoints, knots] = extractNurbsParams(spline);
-nurbs = nrbmak(ctrlPoints, knots);
+function t = ThrustByCurrent(A, b, I)
+t = A*I^b
+return
 end
--- optimization.m
+++ optimization.m
@@ -1,6 +1,17 @@
+global specCapSlineID specCapSpl
+
+specCapSpl=GetNURBSfromSC(specCapSlineID);
+
 options = optimoptions('ga',...
     'ConstraintTolerance',1e-3,...
     'CreationFcn','gacreationuniform',...
-    'MaxGenerations',50,...
+    'PopulationSize',500,...
+    'MaxGenerations',100,...
     'PlotFcn', @gaplotbestf);
-dpmin=ga(@(x) DroneDesign(x),length(dp),[],[],[],[],dprng(1,:),dprng(2,:),[],[6;7;8],options)
+dpmin=ga(@(x) DroneDesign(x),length(dp),[],[],[],[],dprng(1,:),dprng(2,:),[],[5;6;7],options)
+
+function nurbs = GetNURBSfromSC(curveId)
+spline = fetchCurve(curveId);
+[ctrlPoints, knots] = extractNurbsParams(spline);
+nurbs = nrbmak(ctrlPoints, knots);
+end
